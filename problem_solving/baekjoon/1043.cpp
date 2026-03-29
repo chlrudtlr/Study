8b19519f030104ee92f6836255fc0693d809205f
최경식 <116335764+chlrudtlr@users.noreply.github.com>
2026-03-29 05:36:17 +0000

    Solved 1043

--- problem_solving/baekjoon/1043.cpp
+++ problem_solving/baekjoon/1043.cpp
@@ -12,11 +12,11 @@
 
 using namespace std;
 
-int N;        // 사람 수
-int M;        // 파티 수
-int truthNum; // 진실을 아는 사람의 수
-vector<int> truthFriends;
-vector<vector<int>> friendsInParty;
+int N;                              // 사람 수
+int M;                              // 파티 수
+int truthNum;                       // 진실을 아는 사람의 수
+vector<int> truthFriends;           // 진실을 아는 친구들 담은 벡터
+vector<vector<int>> friendsInParty; // 파티 별 참석 인원들 담은 벡터
 
 vector<int> parentFriends;
 vector<int> rankFriends;
@@ -28,7 +28,7 @@ int FindParent(int a)
         return a;
     }
 
-    parentFriends[a] = FindParent(a);
+    parentFriends[a] = FindParent(parentFriends[a]);
     return parentFriends[a];
 }
 
@@ -70,9 +70,9 @@ int main()
 
     // Init
     friendsInParty.resize(M + 2);
-    parentFriends.resize(N, 0);
-    rankFriends.resize(N, 0);
-    for (int i = 0; i < N; i++)
+    parentFriends.resize(N + 2, 0);
+    rankFriends.resize(N + 2, 0);
+    for (int i = 1; i < N + 1; i++) // 1-based
     {
         parentFriends[i] = i;
     }
@@ -96,15 +96,45 @@ int main()
         }
     }
 
+    // Debugging
+    // for (int i = 0; i < M; i++)
+    // {
+    //     cout << i + 1 << "번째 파티 참석자 : ";
+    //     for (int j = 0; j < friendsInParty[i].size(); j++)
+    //     {
+    //         cout << friendsInParty[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
     // Prepare
     map<int, bool> m;
     for (int i = 0; i < truthFriends.size(); i++)
     {
+        // cout << "진실을 알고 있는 자 : " << truthFriends[i] << endl;
+        // cout << "그/그녀의 Parent : " << FindParent(truthFriends[i]) << endl;
         m[FindParent(truthFriends[i])] = true;
     }
 
     // 이제 각 파티를 돌면서 진실쟁이가 없는 파티를 찾으면 된다. 아마도
+    int result = 0;
     for (int i = 0; i < M; i++)
     {
+        bool someone_knows_thuth = false;
+        for (int j = 0; j < friendsInParty[i].size(); j++)
+        {
+            if (m[FindParent(friendsInParty[i][j])] == true)
+            {
+                someone_knows_thuth = true;
+                break;
+            }
+        }
+
+        if (!someone_knows_thuth)
+        {
+            result++;
+        }
     }
+
+    cout << result << endl;
 }
