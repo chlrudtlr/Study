be5086815c148e2f753cd1590866fadbe7b5568b
최경식 <116335764+chlrudtlr@users.noreply.github.com>
2026-03-31 14:15:28 +0000

    priority queue 정리 중

--- stl_study/priority_queue.cpp
+++ stl_study/priority_queue.cpp
@@ -7,6 +7,26 @@
 
 using namespace std;
 
+struct cmp1
+{
+    bool operator()(pair<int, int> a, pair<int, int> b)
+    {
+        if (a.first == b.first)
+            return a.second > b.second; // second 오름차순
+        return a.first < b.first;       // first 내림차순
+    }
+};
+
+struct cmp2
+{
+    bool operator()(pair<int, int> a, pair<int, int> b)
+    {
+        if (a.first == b.first)
+            return a.second > b.second; // second 오름차순
+        return a.first > b.first;       // first 내림차순
+    }
+};
+
 int main()
 {
     cout << "===== 가장 기본적인 사용법 =====" << endl;
@@ -17,7 +37,7 @@ int main()
     pq.push(1);
     pq.push(3);
 
-    cout << "자동으로 내림차순 정렬 : ";
+    cout << "▶ 자동으로 내림차순 정렬 : ";
     while (!pq.empty())
     {
         cout << pq.top() << " ";
@@ -34,7 +54,7 @@ int main()
     pq1.push(1);
     pq1.push(3);
 
-    cout << "이런식으로 하면 오름차순으로 정렬 : ";
+    cout << "▶ 이런 식으로 하면 오름차순으로 정렬 : ";
     while (!pq1.empty())
     {
         cout << pq1.top() << " ";
@@ -51,7 +71,7 @@ int main()
     pq2.push(make_pair(3, 2));
     pq2.push(make_pair(2, 5));
 
-    cout << "첫 번째 원소 기준으로 내림차순 정렬 : ";
+    cout << "▶ 첫 번째 원소 기준으로 내림차순 정렬 : ";
     while (!pq2.empty())
     {
         cout << "(" << pq2.top().first << ", " << pq2.top().second << ")" << " ";
@@ -60,7 +80,7 @@ int main()
     cout << endl;
 
     cout << "그런데 만약 첫 번째 원소가 같으면?" << endl;
-    cout << "두 번째 원소 기준 내림차순 : ";
+    cout << "▶ 두 번째 원소 기준 내림차순 : ";
     pq2.push(make_pair(5, 1));
     pq2.push(make_pair(4, 2));
     pq2.push(make_pair(1, 1));
@@ -70,6 +90,40 @@ int main()
     {
         cout << "(" << pq2.top().first << ", " << pq2.top().second << ")" << " ";
         pq2.pop();
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "그럼 이제 이런 생각이 듭니다." << endl;
+    cout << "첫 번째 원소는 내림차순으로, (첫 번째 원소가 같은 애들은)두 번째 원소는 오름차순으로 정렬하고 싶다!" << endl;
+    cout << "그럴 때에는 comparator를 직접 커스텀해서 사용해야 한다." << endl;
+
+    cout << "▶ 첫 번째 원소는 내림차순, 두 번째 원소는 오름차순 : ";
+    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp1> pq3;
+    pq3.push(make_pair(5, 1));
+    pq3.push(make_pair(4, 2));
+    pq3.push(make_pair(1, 1));
+    pq3.push(make_pair(5, 2));
+    pq3.push(make_pair(5, 3));
+    while (!pq3.empty())
+    {
+        cout << "(" << pq3.top().first << ", " << pq3.top().second << ")" << " ";
+        pq3.pop();
+    }
+    cout << endl;
+
+    cout << "comparator를 잘 정의하면 다음과 같이 첫 번째 원소, 두 번쨰 원소 둘 다 오름차순도 가능" << endl;
+    cout << "▶ 첫 번째 원소랑 두 번째 원소 둘 다 오름차순 : ";
+    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp2> pq4;
+    pq4.push(make_pair(5, 1));
+    pq4.push(make_pair(4, 2));
+    pq4.push(make_pair(1, 1));
+    pq4.push(make_pair(5, 2));
+    pq4.push(make_pair(5, 3));
+    while (!pq4.empty())
+    {
+        cout << "(" << pq4.top().first << ", " << pq4.top().second << ")" << " ";
+        pq4.pop();
     }
     cout << endl;
 
